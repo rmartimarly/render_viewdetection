3331d05113ced1939d62717e3733787569a53756
Robert Martí <37406616+rmartimarly@users.noreply.github.com>
2020-04-21 10:14:34 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/xis3oygt6pkzz4p/viewDetector_resnet_512_export.pkl?dl=0'
+export_file_name = 'viewDetector_resnet_512_export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Frontal', 'Lateral', 'Other']
 path = Path(__file__).parent
 
 app = Starlette()
